dfea2f94539bbfdedf009522e75a0bef02fa95ed
Raj Kishore Sahni <rajkishoresahni826@gmail.com>
2021-06-27 15:53:46 +0530

    added Exception class

--- src/main/java/com/addressBook/Exception/AddressBookException.java
+++ src/main/java/com/addressBook/Exception/AddressBookException.java
@@ -1,5 +1,29 @@
 package com.addressBook.Exception;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.addressBook.DTO.ResponseDTO;
+
+@ControllerAdvice
 public class AddressBookException {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMessage = errorList.stream().map(mapper -> mapper.getDefaultMessage())
+				.collect(Collectors.toList());
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing rest request", errMessage);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+	
+	
 
 }
